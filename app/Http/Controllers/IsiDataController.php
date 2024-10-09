692a14bab4fae97585423367a2423cb611f1f970
Danu <dciptadi@gmail.com>
2024-10-09 16:48:38 +0700

    update 09-10-2024

--- app/Http/Controllers/IsiDataController.php
+++ app/Http/Controllers/IsiDataController.php
@@ -12,10 +12,43 @@ class IsiDataController extends Controller
         $kode_jenis_vendor = session('kode_jenis_vendor');
 
         $refKatDokumenVendor = RefKatDokumenVendor::where('kode_jenis_vendor', $kode_jenis_vendor)
-        ->where('is_main', true)
-        ->orderBy('urutan', 'ASC')
-        ->get();
+            ->where('is_main', true)
+            ->orderBy('urutan', 'ASC')
+            ->get();
 
-        dd($refKatDokumenVendor);
+        if (empty($refKatDokumenVendor)) {
+            return 'data ref_kat_dokumen_vendor tidak ditemukan';
+        }
+
+        $mainStep = [];
+        $subStep = [];
+
+        foreach ($refKatDokumenVendor as $i =>  $data)
+        {
+            $mainStep[] = [
+                'nama_kategori' => $data->nama_kategori,
+            ];
+
+            if (($data->kode_kat_dokumen_vendor == $kode_kat_dokumen_vendor) && ($data->is_has_sub == true)) {
+                $refKatDokumenVendor2 = RefKatDokumenVendor::where('main_kat', $kode_kat_dokumen_vendor)
+                    ->orderBy('urutan', 'ASC')
+                    ->get();
+
+                foreach ($refKatDokumenVendor2 as $i2 => $dataSub) {
+                    $subStep[] = [
+                        'nama_kategori' => $dataSub->nama_kategori,
+                    ];
+                }
+            }
+        }
+
+        $response = [
+            'data' => [
+                'mainStep' => $mainStep,
+                'subStep' => $subStep,
+            ]
+        ];
+
+        dd($response);
     }
 }
