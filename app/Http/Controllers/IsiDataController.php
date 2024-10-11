5c6900a5953ef52d1654287c8be6f3b02715724f
Danu <dciptadi@gmail.com>
2024-10-11 17:19:09 +0700

    update IsiDataController

--- app/Http/Controllers/IsiDataController.php
+++ app/Http/Controllers/IsiDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RefItemTanya;
 use App\Models\RefKatDokumenVendor;
 use Illuminate\Http\Request;
 
@@ -25,12 +26,12 @@ class IsiDataController extends Controller
         $mainLastIndex = count($refKatDokumenVendor) - 1;
 
         $subStep = [];
+        $subStepCount = 1;
 
-        $isLastStep = false;
 
         foreach ($refKatDokumenVendor as $i =>  $data)
         {
-
+            $isLastStep = false;
             if ($i == $mainLastIndex) {
                 $isLastStep = true;
             }
@@ -43,27 +44,59 @@ class IsiDataController extends Controller
 
             $mainStepCount++;
 
+            // Sub Step
             if (($data->kode_kat_dokumen_vendor == $kode_kat_dokumen_vendor) && ($data->is_has_sub == true)) {
                 $refKatDokumenVendor2 = RefKatDokumenVendor::where('main_kat', $kode_kat_dokumen_vendor)
                     ->orderBy('urutan', 'ASC')
                     ->get();
 
+                $subLastIndex = count($refKatDokumenVendor2) - 1;
+
                 foreach ($refKatDokumenVendor2 as $i2 => $dataSub) {
+
+                    $isLastStep = false;
+                    if ($i2 == $subLastIndex) {
+                        $isLastStep = true;
+                    }
+
                     $subStep[] = [
                         'nama_kategori' => $dataSub->nama_kategori,
+                        'stepCount' => $subStepCount,
+                        'isLastStep' => $isLastStep,
                     ];
+
+                    $subStepCount++;
                 }
             }
+        }
+
+        $refItemTanya = RefItemTanya::where('kode_jenis_vendor', $kode_jenis_vendor)
+            ->where('kode_kat_dokumen_vendor', $kode_kat_dokumen_vendor)
+            ->get();
+
+            
+        if (empty($refItemTanya)) {
+            return "Item tanya tidak ditemukan";
+        }
+            
+        $arrItemTanya = [];
+
+        foreach($refItemTanya as $itemTanya) {
+            $arrItemTanya[] = [
+                'nama_item' => $itemTanya->nama_item,
+                'tipe_input' => $itemTanya->tipe_input,
+                'metadata' => $itemTanya->metadata,
+            ];
         }
 
         $data = [
             'mainSteps' => $mainStep,
             'subSteps' => $subStep,
+            'itemTanya' => $arrItemTanya,
         ];
 
-        // dd($data);
+        dd($data);
 
         return view('maincontent/isidata', $data);
-
     }
 }
--- resources/views/maincontent/isidata.blade.php
+++ resources/views/maincontent/isidata.blade.php
@@ -17,11 +17,11 @@
         <div class="container mx-auto">
 
             <!-- Stepper -->
-            <div class="flex justify-between items-center">
+            <div class="flex justify-between items-center mb-8">
 
                 @foreach ($mainSteps as $mainStep)
 
-                <!-- Step -->
+                <!-- Main Step -->
                 <div class="flex flex-col items-center">
                     <div class="flex items-center justify-center w-10 h-10 bg-blue-500 text-white rounded-full">
                         {{ $mainStep['stepCount'] }}
@@ -34,8 +34,31 @@
                 <div class="flex-auto border-t-2 border-blue-500 mx-4"></div>
                 @endif
 
-
                 @endforeach
+
+            </div>
+
+            <div class="flex justify-between items-center">
+
+                @if (!empty($subSteps))
+                    @foreach ($subSteps as $subStep)
+                    <!-- Sub Step -->
+                    <div class="flex flex-col items-center">
+                        <div class="flex items-center justify-center w-10 h-10 bg-blue-500 text-white rounded-full">
+                            {{ $subStep['stepCount'] }}
+                        </div>
+                        <span class="mt-2 text-blue-500">{{ $subStep['nama_kategori'] }}</span>
+                    </div>
+
+                    @if ($subStep['isLastStep'] == false)
+                    <!-- Line Between Steps -->
+                    <div class="flex-auto border-t-2 border-blue-500 mx-4"></div>
+                    @endif
+
+                    @endforeach
+
+                @endif
+
             </div>
 
         </div>
